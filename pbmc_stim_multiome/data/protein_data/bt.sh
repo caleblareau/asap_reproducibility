39c6a9aef42c4736151aefdcfb342b46cb266629
caleblareau <clareau@stanford.edu>
2021-01-20 16:57:15 -0800

    misc

---
+++ pbmc_stim_multiome/data/protein_data/bt.sh
@@ -0,0 +1,4 @@
+bustools text -o LLL_ctrl_mo.bustools.tsv LLL_ctrl_kite/output_corrected.bus
+bustools text -o LLL_stim_mo.bustools.tsv LLL_stim_kite/output_corrected.bus
+bustools text -o DIG_ctrl_mo.bustools.tsv DIG_ctrl_kite/output_corrected.bus
+bustools text -o DIG_stim_mo.bustools.tsv DIG_stim_kite/output_corrected.bus
